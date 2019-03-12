ecb8eae208b60ebb37d16f52fffd5eba5adcb504
Jong in Park <whddls9632@naver.com>
2019-03-13 05:39:33 +0900

    2048 (EASY) 백준 코드

--- brute/bitmask/12100.cpp
+++ brute/bitmask/12100.cpp
@@ -1,217 +1,189 @@
 // 2048 (EASY)
+// 백준's 코드
 #include <iostream>
-#include <queue>
+#include <cassert>
 #include <vector>
+#include <string>
 using namespace std;
-int n;
-int ans = 0;
-
-int move(vector<vector<int>> &b, int d)
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+const int LIMIT = 5;
+vector<int> gen(int k)
 {
-	cout << d << " \n";
-	int best = 0;
-	if(d == 0) // 위
-	{
-		for(int i = 0; i < n; i++)
-		{
-			bool find = false;
-			queue<int> tq;
-			int cur = -1;
-			for(int j = n-1; j >= 0; j--)
-			{
-				if(b[j][i] == 0) continue;
-				if(find)
-				{
-					if(cur == b[j][i])
-					{
-						b[j][i] *= 2;
-						tq.push(b[j][i]);
-						find = false;
-					}
-					else
-					{
-						tq.push(cur);
-						cur = b[j][i];
-					}
-				}
-				else
-				{
-					cur = b[j][i];
-					find = true;
-				}
-			}
-			if( ( i == n-1 ) && ( tq.back() != cur ) ) tq.push(cur);
-			for(int j = 0; j < n; j++)
-			{
-				if(!tq.empty())
-				{
-					auto temp = tq.front();
-					if(temp > best) best = temp;
-					tq.pop();
-					b[j][i] = temp;
-				}
-				else b[j][i] = 0;
-			}
-		}
-	}
-	else if(d == 1) // 아래
-	{
-		for(int i = 0; i < n; i++)
-		{
-			bool find = false;
-			queue<int> tq;
-			int cur = -1;
-			for(int j = 0; j < n; j++)
-			{
-				if(b[j][i] == 0) continue;
-				if(find)
-				{
-					if(cur == b[j][i])
-					{
-						b[j][i] *= 2;
-						tq.push(b[j][i]);
-						find = false;
-					}
-					else
-					{
-						tq.push(cur);
-						cur = b[j][i];
-					}
-				}
-				else
-				{
-					cur = b[j][i];
-					find = true;
-				}
-			}
-			if( ( i == n-1 ) && ( tq.back() != cur ) ) tq.push(cur);
-			for(int j = n-1; j >= 0; j--)
-			{
-				if(!tq.empty())
-				{
-					auto temp = tq.front();
-					if(temp > best) best = temp;
-					tq.pop();
-					b[j][i] = temp;
-				}
-				else b[j][i] = 0;
-			}
-		}
-	}
-	else if(d == 2) // 오른쪽
-	{
-		for(int i = 0; i < n; i++)
-		{
-			bool find = false;
-			queue<int> tq;
-			int cur = -1;
-			for(int j = 0; j < n; j++)
-			{
-				if(b[i][j] == 0) continue;
-				if(find)
-				{
-					if(cur == b[i][j])
-					{
-						b[i][j] *= 2;
-						tq.push(b[i][j]);
-						find = false;
-					}
-					else
-					{
-						tq.push(cur);
-						cur = b[i][j];
-					}
-				}
-				else
-				{
-					cur = b[i][j];
-					find = true;
-				}
-			}
-			if( ( i == n-1 ) && ( tq.back() != cur ) ) tq.push(cur);
-			for(int j = n-1; j >= 0; j--)
-			{
-				if(!tq.empty())
-				{
-					auto temp = tq.front();
-					if(temp > best) best = temp;
-					tq.pop();
-					b[i][j] = temp;
-				}
-				else b[i][j] = 0;
-			}
-		}
-	}
-	else // 왼쪽
-	{
-		for(int i = 0; i < n; i++)
-		{
-			bool find = false;
-			queue<int> tq;
-			int cur = -1;
-			for(int j = n-1; j >= 0; j--)
-			{
-				if(b[i][j] == 0) continue;
-				if(find)
-				{
-					if(cur == b[i][j])
-					{
-						b[i][j] *= 2;
-						tq.push(b[i][j]);
-						find = false;
-					}
-					else
-					{
-						tq.push(cur);
-						cur = b[i][j];
-					}
-				}
-				else
-				{
-					cur = b[i][j];
-					find = true;
-				}
-			}
-			if( ( i == n-1 ) && ( tq.back() != cur ) ) tq.push(cur);
-			for(int j = 0; j < n; j++)
-			{
-				if(!tq.empty())
-				{
-					auto temp = tq.front();
-					if(temp > best) best = temp;
-					tq.pop();
-					b[i][j] = temp;
-				}
-				else b[i][j] = 0;
-			}
-		}
-	}
-	return best;
+    vector<int> a(LIMIT);
+    for (int i=0; i<LIMIT; i++)
+    {
+        a[i] = (k&3);
+        k >>= 2;
+    }
+    return a;
 }
-
-int main(void)
+void print(vector<vector<pair<int,bool>>> &a)
 {
-	cin >> n;
-	vector<vector<int>> board(n, vector<int> (n));
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j < n; j++)
-			cin >> board[i][j];
-
-	for(int i1 = 0; i1 < 4; i1++)
-		for(int i2 = 0; i2 < 4; i2++)
-			for(int i3 = 0; i3 < 4; i3++)
-				for(int i4 = 0; i4 < 4; i4++)
-					for(int i5 = 0; i5 < 4; i5++)
-					{
-						vector<vector<int>> temp(n, vector<int> (n));
-						temp = board;
-						move(temp,i1);
-						move(temp,i2);
-						move(temp,i3);
-						move(temp,i4);
-						int t = move(temp,i5);
-						if(t > ans) ans = t;
-					}
-	cout << ans << "\n";
-	return 0;
+    int n = a.size();
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<n; j++)
+            cout << a[i][j].first << ' ';
+        cout << '\n';
+    }
+}
+int check(vector<vector<int>> &a, vector<int> &dirs)
+{
+    int n = a.size();
+    vector<vector<pair<int,bool>>> d(n, vector<pair<int,bool>>(n));
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<n; j++)
+            d[i][j].first = a[i][j];
+    }
+    // 0: down, 1: up, 2: left, 3: right
+    for (int dir : dirs)
+    {
+        bool ok = false;
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<n; j++)
+                d[i][j].second = false;
+        }
+        while (true)
+        {
+            ok = false;
+            if (dir == 0)
+            {
+                for (int i=n-2; i>=0; i--)
+                {
+                    for (int j=0; j<n; j++)
+                    {
+                        if (d[i][j].first == 0) continue;
+                        if (d[i+1][j].first == 0)
+                        {
+                            d[i+1][j].first = d[i][j].first;
+                            d[i+1][j].second = d[i][j].second;
+                            d[i][j].first = 0;
+                            ok = true;
+                        }
+                        else if (d[i+1][j].first == d[i][j].first)
+                        {
+                            if (d[i][j].second == false && d[i+1][j].second == false)
+                            {
+                                d[i+1][j].first *= 2;
+                                d[i+1][j].second = true;
+                                d[i][j].first = 0;
+                                ok = true;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (dir == 1)
+            {
+                for (int i=1; i<n; i++)
+                {
+                    for (int j=0; j<n; j++)
+                    {
+                        if (d[i][j].first == 0) continue;
+                        if (d[i-1][j].first == 0)
+                        {
+                            d[i-1][j].first = d[i][j].first;
+                            d[i-1][j].second = d[i][j].second;
+                            d[i][j].first = 0;
+                            ok = true;
+                        }
+                        else if (d[i-1][j].first == d[i][j].first)
+                        {
+                            if (d[i][j].second == false && d[i-1][j].second == false)
+                            {
+                                d[i-1][j].first *= 2;
+                                d[i-1][j].second = true;
+                                d[i][j].first = 0;
+                                ok = true;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (dir == 2)
+            {
+                for (int j=1; j<n; j++)
+                {
+                    for (int i=0; i<n; i++)
+                    {
+                        if (d[i][j].first == 0) continue;
+                        if (d[i][j-1].first == 0)
+                        {
+                            d[i][j-1].first = d[i][j].first;
+                            d[i][j-1].second = d[i][j].second;
+                            d[i][j].first = 0;
+                            ok = true;
+                        }
+                        else if (d[i][j-1].first == d[i][j].first)
+                        {
+                            if (d[i][j].second == false && d[i][j-1].second == false)
+                            {
+                                d[i][j-1].first *= 2;
+                                d[i][j-1].second = true;
+                                d[i][j].first = 0;
+                                ok = true;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (dir == 3)
+            {
+                for (int j=n-2; j>=0; j--)
+                {
+                    for (int i=0; i<n; i++)
+                    {
+                        if (d[i][j].first == 0) continue;
+                        if (d[i][j+1].first == 0)
+                        {
+                            d[i][j+1].first = d[i][j].first;
+                            d[i][j+1].second = d[i][j].second;
+                            d[i][j].first = 0;
+                            ok = true;
+                        }
+                        else if (d[i][j+1].first == d[i][j].first)
+                        {
+                            if (d[i][j].second == false && d[i][j+1].second == false)
+                            {
+                                d[i][j+1].first *= 2;
+                                d[i][j+1].second = true;
+                                d[i][j].first = 0;
+                                ok = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (ok == false) break;
+        }
+    }
+    int ans = 0;
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<n; j++)
+            if (ans < d[i][j].first)
+                ans = d[i][j].first;
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> a(n, vector<int> (n));
+    for (int i=0; i<n; i++)
+        for (int j=0; j<n; j++)
+            cin >> a[i][j];
+    int ans = 0;
+    for (int k=0; k<(1<<(LIMIT*2)); k++)
+    {
+        vector<int> dir = gen(k);
+        int cur = check(a, dir);
+        if (ans < cur) ans = cur;
+    }
+    cout << ans << '\n';
+    return 0;
 }
