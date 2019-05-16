4c7f1bb60dfd4c45cbccecf13bb8562cb549290d
Jong in Park <whddls9632@naver.com>
2019-05-16 12:55:29 +0900

    simulation problem third - first

---
+++ simulation/14503.cpp
@@ -0,0 +1,52 @@
+// 로봇 청소기
+// 백준's 코드
+#include <iostream>
+using namespace std;
+int dx[] = {-1,0,1,0};
+int dy[] = {0,1,0,-1};
+int a[55][55];
+int main(void)
+{
+	int n, m;
+	cin >> n >> m;
+	int x, y, dir;
+	cin >> x >> y >> dir;
+	for (int i=0; i<n; i++)
+		for (int j=0; j<m; j++)
+			cin >> a[i][j];
+	int cnt = 0;
+	while (true)
+	{
+		if (a[x][y] == 0)
+			a[x][y] = 2;
+		if (a[x+1][y] != 0 && a[x-1][y] != 0 && a[x][y-1] != 0 && a[x][y+1] != 0)
+		{
+			if (a[x-dx[dir]][y-dy[dir]] == 1)
+				break;
+			else
+			{
+				x -= dx[dir];
+				y -= dy[dir];
+			}
+		}
+		else
+		{
+			dir = (dir + 3) % 4;
+			if (a[x+dx[dir]][y+dy[dir]] == 0)
+			{
+				x += dx[dir];
+				y += dy[dir];
+			}
+		}
+	}
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<m; j++)
+		{
+			if (a[i][j] == 2)
+				cnt += 1;
+		}
+	}
+	cout << cnt << '\n';
+	return 0;
+}
