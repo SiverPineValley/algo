ff96bfa84d3d013cd7be7201a7ee08197539c54c
Jong in Park <whddls9632@naver.com>
2019-08-12 17:24:57 +0900

    BOJ14502 Complete

--- Samsung SW Expert Academy/SW Test/BOJ14502.cpp
+++ Samsung SW Expert Academy/SW Test/BOJ14502.cpp
@@ -2,18 +2,22 @@
 #include <iostream>
 #include <utility>
 #include <queue>
-
+#include <cstring>
 using namespace std;
 
 #define MAXSIZE 8
 
 int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
+int map[MAXSIZE][MAXSIZE];
 int numOfVirusAndWall = 0;		// 초기 Map에서 Virus와 Wall의 개수
+int result = 0;
 int n, m;
 
-int simulation(int map[MAXSIZE][MAXSIZE], queue<pair<int,int>> virus)
+int simulation(queue<pair<int,int>> virus)
 {
+	int tempMap[MAXSIZE][MAXSIZE];
+	memcpy(tempMap, map, sizeof(map));
 	int blank = 0;
 
 	while(!virus.empty())
@@ -29,9 +33,9 @@ int simulation(int map[MAXSIZE][MAXSIZE], queue<pair<int,int>> virus)
 
 			if(0 <= nx && nx < m && 0 <= ny && ny < n)
 			{
-				if(map[ny][nx] == 0)
+				if(tempMap[ny][nx] == 0)
 				{
-					map[ny][nx] = 2;
+					tempMap[ny][nx] = 2;
 					virus.push(make_pair(nx,ny));
 					blank++;
 				}
@@ -41,10 +45,29 @@ int simulation(int map[MAXSIZE][MAXSIZE], queue<pair<int,int>> virus)
 	return blank;
 }
 
+void bfs(queue<pair<int,int>> virus, int stage, int startY)
+{
+	if(stage == 3)
+	{
+		int sim = simulation(virus);
+		int simResult = n*m - numOfVirusAndWall - sim - 3;
+		if(simResult > result)
+			result = simResult;
+		return;
+	}
+	for(int y = startY; y < n; y++)
+		for(int x = 0; x < m; x++)
+		{
+			if(map[y][x] != 0) continue;
+			map[y][x] = 1;
+			bfs(virus, stage + 1, y);
+			map[y][x] = 0;
+		}
+}
+
 int main(void)
 {
 	cin >> n >> m;
-	int map[MAXSIZE][MAXSIZE];
 	queue<pair<int,int>> virus;
 
 	for(int row = 0; row < n; row++)
@@ -61,7 +84,8 @@ int main(void)
 		}
 	}
 
-	cout << simulation(map, virus) << "\n";
+	bfs(virus, 0, 0);
+	cout << result << "\n";
 
 	return 0;
 }
